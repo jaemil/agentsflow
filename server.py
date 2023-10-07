7bdb509d403c004b9c5579732a8cf25f5c1b672f
TomExMachina <tom@firesight.ai>
2023-10-07 01:15:15 -0400

    Serve the main page.

--- server.py
+++ server.py
@@ -3,7 +3,9 @@ import logging
 import queue
 from functools import partial
 
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from autogen_client.initialize import run_agent
 from autogen_client.config import config
 from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
@@ -19,11 +21,13 @@ manager = Manager()
 send_queue = manager.Queue()
 receive_queue = manager.Queue()
 
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-async def index():
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
     # serve view here
-    pass
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
 @app.websocket("/ws")
