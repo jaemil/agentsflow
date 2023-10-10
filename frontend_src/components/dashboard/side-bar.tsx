64c59030f50d5bbbd20f01dc863a98a86f4fbb5c
jaemil <emilhopf@gmail.com>
2023-10-10 03:15:29 +0200

    small changes

--- frontend_src/components/dashboard/side-bar.tsx
+++ frontend_src/components/dashboard/side-bar.tsx
@@ -3,10 +3,21 @@ import React from "react";
 import { Button } from "../ui/button";
 import useStore from "@/lib/store";
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import SettingsForm from "./settings-form";
+
 export default function SideBar() {
   const setOpenChat = useStore((state) => state.setOpenChat);
+
   return (
-    <div className="w-48 h-full p-4 border-r">
+    <div className="flex flex-col justify-between w-48 h-full p-4 border-r">
       <div className="space-y-1">
         <Button
           onClick={() => setOpenChat(false)}
@@ -71,6 +82,35 @@ export default function SideBar() {
           Files
         </Button>
       </div>
+
+      <Dialog>
+        <DialogTrigger className="justify-start w-full">
+          <div className="inline-flex items-center justify-start w-full h-10 px-4 py-2 text-sm font-medium transition-colors rounded-md ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="15"
+              height="15"
+              viewBox="0 0 15 15"
+              className="w-4 h-4 mr-2"
+            >
+              <path
+                id="Icon_ionic-md-settings"
+                data-name="Icon ionic-md-settings"
+                d="M16.571,11.625a4.69,4.69,0,0,0,.038-.75c0-.263-.038-.487-.038-.75l1.611-1.237a.344.344,0,0,0,.077-.487L16.724,5.813a.374.374,0,0,0-.46-.15l-1.918.75a5.582,5.582,0,0,0-1.3-.75l-.269-1.987a.411.411,0,0,0-.384-.3H9.321a.411.411,0,0,0-.384.3L8.631,5.662a6.5,6.5,0,0,0-1.3.75l-1.918-.75a.359.359,0,0,0-.46.15L3.415,8.4a.453.453,0,0,0,.077.487l1.649,1.237c0,.263-.038.487-.038.75s.038.487.038.75L3.53,12.862a.344.344,0,0,0-.077.487l1.534,2.588a.374.374,0,0,0,.46.15l1.918-.75a5.582,5.582,0,0,0,1.3.75l.307,1.987a.372.372,0,0,0,.384.3h3.068a.411.411,0,0,0,.384-.3l.307-1.987a6.491,6.491,0,0,0,1.3-.75l1.918.75a.359.359,0,0,0,.46-.15l1.534-2.588a.452.452,0,0,0-.077-.487ZM10.856,13.5a2.626,2.626,0,1,1,2.685-2.625A2.639,2.639,0,0,1,10.856,13.5Z"
+                transform="translate(-3.375 -3.375)"
+                fill="currentColor"
+              />
+            </svg>
+            Settings
+          </div>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Settings</DialogTitle>
+          </DialogHeader>
+          <SettingsForm />
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
